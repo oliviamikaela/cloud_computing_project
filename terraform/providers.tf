cf44ac65b9b40c7efe4a90d2bc8d0d08b5a66226
Lotta Rydin <lotta.rydin@telia.com>
2021-10-15 11:48:50 +0200

    init and plan is working. Apply is not working. I think it needs to be fixed in provider in main.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,1 +1,13 @@
-provider "openstack" {}
+# This was not in the guide. Is needed
+terraform {
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+      version = "1.44.0"
+    }
+  }
+}
+
+provider "openstack" {
+    auth_url = "https://east-1.cloud.snic.se:8004/v1/c32d121b91b648738e64d50490c05505" # Auth_ur was need. Taken from https://east-1.cloud.snic.se/project/api_access/
+}
