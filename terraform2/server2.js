6de0e3c6ebbe482f6446550fcdf58a1541c8b1dd
Ammar <ammar.h.d@hotmail.com>
2021-10-26 14:46:07 +0200

    ..

--- terraform2/server2.js
+++
@@ -1,8 +0,0 @@
-const exec = require("child_process").exec;
-const child = exec("sh ./run_terraform.sh ", (error, stdout, stderr) => {
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-  if (error !== null) {
-    console.log(`exec error: ${error}`);
-  }
-});
